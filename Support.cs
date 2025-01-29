caaf79a28737630a8359ffcf02c5c65c508f8d44
aaronwshaw <ashaw32@byu.edu>
2025-01-29 11:39:00 -0700

    Support branch

--- Support.cs
+++ Support.cs
@@ -2,5 +2,111 @@ namespace Mission4;
 
 public class Support
 {
-    
+    public void printBoard(string[,] board)
+    {
+        for (int row = 0; row < 3; row++)
+        {
+            for (int col = 0; col < 3; col++)
+            {
+                if (col == 2)
+                {
+                    Console.Write(board[row, col]);
+                }
+                else
+                {
+                    Console.Write(board[row, col] + " | ");
+                }
+
+            }
+
+            Console.WriteLine();
+
+        }
+    }
+
+    public (bool winnerExists, string winner, bool full) checkWinner(string[,] board)
+    {
+        int full = 0;
+
+        // Check rows for winner
+        string results;
+        for (int row = 0; row < 3; row++)
+        {
+            results = "";
+            for (int col = 0; col < 3; col++)
+            {
+                results += board[row, col];
+
+            }
+            if (results.Equals("XXX"))
+            {
+                return (true, "X", false);
+            }
+            else if (results.Equals("OOO"))
+            {
+                return (true, "O", false);
+            }
+
+            if (!results.Contains("-"))
+            {
+                full++;
+            }
+        }
+
+        // Check columns for winner
+        for (int col = 0; col < 3; col++)
+        {
+            results = "";
+            for (int row = 0; row < 3; row++)
+            {
+                results += board[row, col];
+
+            }
+
+            if (results.Equals("XXX"))
+            {
+                return (true, "X", false);
+            }
+            else if (results.Equals("OOO"))
+            {
+                return (true, "O", false);
+            }
+
+        }
+
+        //Check Diagonals for winner
+        results = board[0, 0] + board[1, 1] + board[2, 2];
+
+        if (results.Equals("XXX"))
+        {
+            return (true, "X", false);
+        }
+        else if (results.Equals("OOO"))
+        {
+            return (true, "O", false);
+        }
+
+        //Check other diagonal
+        results = board[0, 2] + board[1, 1] + board[2, 0];
+
+        if (results.Equals("XXX"))
+        {
+            return (true, "X", false);
+        }
+        else if (results.Equals("OOO"))
+        {
+            return (true, "O", false);
+        }
+
+        //Check to see if the board is full
+        if (full == 3)
+        {
+            return (false, "CAT'S GAME", true);
+        }
+        else
+        {
+            return (false, "NA", false);
+        }
+
+    }
 }
