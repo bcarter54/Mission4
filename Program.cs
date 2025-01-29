9dc0e90dce19302dbafa33853bbf62df772c791f
LiterallyMe-kun <sknoah2002@gmail.com>
2025-01-29 10:48:06 -0700

    woooooo

--- Program.cs
+++ Program.cs
@@ -17,17 +17,67 @@ internal class Program
                 board[i, j] = "-";
             }
         }
-        for (int i = 0; i < 3; i++)
+        bool ugh = true;
+         do
         {
-            for (int j = 0; j < 3; j++)
+
+            // ignore our superior print
+            for (int i = 0; i < 3; i++)
             {
-                Console.WriteLine(board[i, j]);
+                string silliness = "";
+                for (int j = 0; j < 3; j++)
+                {
+                    silliness = silliness + " " + i + "," + j + board[i, j];
+
+                    if (j == 2)
+                    {
+                        Console.WriteLine(silliness);
+                    }
+                }
             }
-        }
-        // ask each player in turn for input
 
-        //print board with support class' method
 
-        //check for (and notify) winner using support method
+            // ask each player in turn for input
+
+            Console.WriteLine("Player X, choose your space.");
+        bruh:
+            Console.WriteLine("Left coordinate:");
+            int left = int.Parse(Console.ReadLine());
+            Console.WriteLine("Right coordinate:");
+            int right = int.Parse(Console.ReadLine());
+
+            if (board[left, right] != "-")
+            {
+                Console.WriteLine("Choose a different space.");
+                goto bruh;
+            }
+            else
+            {
+                board[left, right] = "X";
+            }
+
+            //    //print board with support class' method
+            //      sus.PrintBoard(board);
+
+            Console.WriteLine("Player O, choose your space.");
+        cringe:
+            Console.WriteLine("Left coordinate:");
+            left = int.Parse(Console.ReadLine());
+            Console.WriteLine("Right coordinate:");
+            right = int.Parse(Console.ReadLine());
+
+            if (board[left, right] != "-")
+            {
+                Console.WriteLine("Choose a different space.");
+                goto cringe;
+            }
+            else
+            {
+                board[left, right] = "O";
+            }
+
+            //    //check for (and notify) winner using support method
+            //    sus.CheckWinner();
+        } while (ugh == true) ;
     }
 }
