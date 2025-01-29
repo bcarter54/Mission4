6896d62d26badb6670f3650e2cc88d038786faaa
LiterallyMe-kun <sknoah2002@gmail.com>
2025-01-29 09:45:44 -0700

    for madi

--- Program.cs
+++ Program.cs
@@ -1,3 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
+using Mission4;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Support sus = new Support();
 
-Console.WriteLine("Hello, World!");
+        // welcome the user
+        Console.WriteLine("Welcome to Tic-Tac-Toe!");
+        // 2D array, but swag
+        string[,] board = new string[3, 3];
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                board[i, j] = "-";
+            }
+        }
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                Console.WriteLine(board[i, j]);
+            }
+        }
+        // ask each player in turn for input
+
+        //print board with support class' method
+
+        //check for (and notify) winner using support method
+    }
+}
